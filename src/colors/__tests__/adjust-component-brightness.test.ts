86d88a5c23c09f0a57f9f0d32f4774468e5f37d5
adhitya yosua sanjaya <adhityasnjy@gmail.com>
2024-12-28 00:38:01 +0700

    feat(utils): generate contrast color - calculateLuminance - calculate luminance of rgb to determine if its dark or light color - calculate luminance test - isDark - is dark test - isLight - is light test - adjustComponentBrightness - to handle adjusting rgb component brightness - adjust component brightness test - generateContrastColor - to generate contrast color from given rgb object - generate contrast color test - colorizeLog - helper function to colorize stdout printed on cli - colorize log test

---
+++ src/colors/__tests__/adjust-component-brightness.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from 'bun:test'
+import { adjustComponentBrightness } from '../adjust-component-brightness'
+
+test('adjustComponentBrightness with factor', () => {
+  expect(adjustComponentBrightness(0, 0.5)).toBe(0)
+  expect(adjustComponentBrightness(255, 0.5)).toBe(128)
+  expect(adjustComponentBrightness(128, 0.5)).toBe(64)
+})
+
+test('adjustComponentBrightness with offset', () => {
+  expect(adjustComponentBrightness(0, 1, 50)).toBe(50)
+  expect(adjustComponentBrightness(255, 1, 50)).toBe(255)
+  expect(adjustComponentBrightness(128, 1, 50)).toBe(178)
+})
+
+test('adjustComponentBrightness with factor and offset', () => {
+  expect(adjustComponentBrightness(0, 0.5, 50)).toBe(50)
+  expect(adjustComponentBrightness(255, 0.5, 50)).toBe(178)
+  expect(adjustComponentBrightness(128, 0.5, 50)).toBe(114)
+})
---
+++ src/colors/__tests__/calculate-luminance.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'bun:test'
+import { calculateLuminance } from '../calculate-luminance'
+
+test('calculateLuminance on black and white', () => {
+  expect(calculateLuminance({ r: 0, g: 0, b: 0 })).toBeLessThanOrEqual(0.5)
+  expect(calculateLuminance({ r: 255, g: 255, b: 255 })).toBeGreaterThan(0.5)
+})
